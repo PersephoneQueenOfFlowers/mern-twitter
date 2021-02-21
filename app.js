97d8664124cdc775da5e55feaea492c4ca22c08c
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-21 14:24:59 -0800

    validations

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const mongoose = require('mongoose');
 const express = require("express");
 const db = require('./config/keys').mongoURI;
 const app = express();
-app.get("/", (req, res) => res.send("Howdo World"));
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//   console.log(res);
+//   res.send("Howdo World");
+// });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
@@ -10,6 +27,8 @@ mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+
 
 
 
